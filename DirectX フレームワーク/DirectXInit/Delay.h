65108beb1354ade2571c0ec99eb89d8645c7a37b
gitabc0 <gitabc000@gmail.com>
2025-01-23 11:56:50 +0900

    小松：作業
    Delay作った

---
+++ DirectX フレームワーク/DirectXInit/Delay.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Object.h"
+class Delay : public Object
+{
+private:
+	std::vector<unsigned> signals;
+	bool sig;
+
+public:
+	Delay() : sig(false) {}
+	~Delay() {}
+
+	// 更新処理
+	void Update();
+
+	// 説明：任意のフレーム数後に信号を送る
+	// 構文：{Delayオブジェクト}->SetSignal({任意のフレーム数})
+	// 例文：delay->SetSignal(10 * FPS);
+	void SetSignal(unsigned int _frameCount);
+	// 説明：送られた信号を受け取る
+	// 構文：if({Delayオブジェクト}->GetSignal())
+	// 例文：if(delay->GetSignal())
+	bool GetSignal() const;
+};
+
