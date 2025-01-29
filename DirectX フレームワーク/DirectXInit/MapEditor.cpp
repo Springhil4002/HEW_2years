760aec6c84886e6280044d1dae90e5a14f69629b
springhil <nogenora69@gmail.com>
2025-01-29 15:43:25 +0900

    春岡:作業
    ・Stage3の仮作成

--- DirectX フレームワーク/DirectXInit/MapEditor.cpp
+++ DirectX フレームワーク/DirectXInit/MapEditor.cpp
@@ -19,7 +19,7 @@ void MapEditor::Init()
 	cursor->SetPos(0.0f, 0.0f, 0.0f);
 	cursor->SetScale(60.0f, 60.0f, 0.0f);
 
-	Load("TEST10_STAGE2.csv");
+	//Load("TEST10_STAGE2.csv");
 
 	//auto katen = Object::Create<Quad>();
 	//katen->SetTex("asset/Texture/Band_Block.png");
