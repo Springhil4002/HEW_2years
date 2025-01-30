ae43962dfba8584eb17fc338dc231dbffbfcca19
springhil <nogenora69@gmail.com>
2025-01-30 14:01:19 +0900

    春岡:作業
    ・ステージ8のロード書いた

--- DirectX フレームワーク/DirectXInit/GameScene.cpp
+++ DirectX フレームワーク/DirectXInit/GameScene.cpp
@@ -384,6 +384,7 @@ void GameScene::Init(int _num)
 		fade->layer = 10;										// レイヤーを設定
 
 		// Stage8のCSV読み込み
+		MapEditor::Load("Stage8.csv");
 
 		// コイン獲得数オブジェクトの作成
 		auto coinNum1 = Object::Create<CoinNum>();
